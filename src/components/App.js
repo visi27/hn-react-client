362e650c5037d5ac9e6bbdff90f5c225eb6673f4
Evis Bregu <evis.bregu@gmail.com>
2018-02-23 15:39:38 +0100

    Handling api errors

--- src/components/App.js
+++ src/components/App.js
@@ -1,46 +1,46 @@
-import React, { Component } from 'react'
-import Search from './Search'
-import Table from './Table'
-import './App.css'
-import Button from './Button'
+import React, { Component } from "react";
+import Search from "./Search";
+import Table from "./Table";
+import "./App.css";
+import Button from "./Button";
 
-const DEFAULT_QUERY = 'redux'
-const DEFAULT_HPP = '100';
+const DEFAULT_QUERY = "redux";
+const DEFAULT_HPP = "100";
 
-const PATH_BASE = 'https://hn.algolia.com/api/v1'
-const PATH_SEARCH = '/search'
-const PARAM_SEARCH = 'query='
-const PARAM_PAGE = "page="
-const PARAM_HPP = 'hitsPerPage=';
-
+const PATH_BASE = "https://hn.algolia.com/api/v1";
+const PATH_SEARCH = "/search";
+const PARAM_SEARCH = "query=";
+const PARAM_PAGE = "page=";
+const PARAM_HPP = "hitsPerPage=";
 
 class App extends Component {
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
       searchTerm: DEFAULT_QUERY,
       currentPage: 0,
-      cache: {}
-    }
+      cache: {},
+      error: null
+    };
 
-    this.setSearchTopStories = this.setSearchTopStories.bind(this)
-    this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this)
-    this.onDismiss = this.onDismiss.bind(this)
-    this.onSearchChange = this.onSearchChange.bind(this)
-    this.onSearchSubmit = this.onSearchSubmit.bind(this)
-    this.onReset = this.onReset.bind(this)
-    this.nextPage = this.nextPage.bind(this)
+    this.setSearchTopStories = this.setSearchTopStories.bind(this);
+    this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this);
+    this.onDismiss = this.onDismiss.bind(this);
+    this.onSearchChange = this.onSearchChange.bind(this);
+    this.onSearchSubmit = this.onSearchSubmit.bind(this);
+    this.onReset = this.onReset.bind(this);
+    this.nextPage = this.nextPage.bind(this);
   }
 
-  componentDidMount () {
-    const {searchTerm, currentPage} = this.state
-    this.fetchSearchTopStories(searchTerm, currentPage)
+  componentDidMount() {
+    const { searchTerm, currentPage } = this.state;
+    this.fetchSearchTopStories(searchTerm, currentPage);
   }
 
-  render () {
-    const {searchTerm, result, currentPage} = this.state
+  render() {
+    const { searchTerm, result, currentPage, error } = this.state;
 
-    const totPages = result ?  result.nbPages : 0
+    const totPages = result ? result.nbPages : 0;
 
     return (
       <div className="page">
@@ -54,97 +54,99 @@ class App extends Component {
             Search
           </Search>
         </div>
-        {
-          result &&
-          <Table
-            list={result.hits}
-            searchTerm={searchTerm}
-            onDismiss={this.onDismiss}
-          />
-        }
+        {error ? (
+          <div className="interactions">
+            <p>Application Error: {error.message}</p>
+          </div>
+        ) : (
+          result && (
+            <Table
+              list={result.hits}
+              searchTerm={searchTerm}
+              onDismiss={this.onDismiss}
+            />
+          )
+        )}
 
-        {
-          currentPage < totPages
-          && <Button onClick={this.nextPage}> More Articles </Button>
-        }
+        {currentPage < totPages && (
+          <Button onClick={this.nextPage}> More Articles </Button>
+        )}
       </div>
-    )
+    );
   }
 
-  nextPage () {
-    const { searchTerm, currentPage, result } = this.state
-    const totPages = result.nbPages
+  nextPage() {
+    const { searchTerm, currentPage, result } = this.state;
+    const totPages = result.nbPages;
 
-    if(currentPage < totPages) {
+    if (currentPage < totPages) {
       this.setState({
         currentPage: currentPage + 1
-      })
+      });
     }
 
-    this.fetchSearchTopStories(searchTerm, currentPage + 1)
+    this.fetchSearchTopStories(searchTerm, currentPage + 1);
   }
 
-  setSearchTopStories (result, updateCache = false) {
-    const { hits, page, nbPages } = result
-    const oldHits = page !== 0 ? this.state.result.hits : []
-    const updatedHits = [
-      ...oldHits,
-      ...hits
-    ];
+  setSearchTopStories(result, updateCache = false) {
+    const { hits, page, nbPages } = result;
+    const oldHits = page !== 0 ? this.state.result.hits : [];
+    const updatedHits = [...oldHits, ...hits];
 
-    let { cache } = this.state
-    if(updateCache) {
-      if(!cache[this.state.searchTerm]) {
-        cache[this.state.searchTerm] = {pages: []}
+    let { cache } = this.state;
+    if (updateCache) {
+      if (!cache[this.state.searchTerm]) {
+        cache[this.state.searchTerm] = { pages: [] };
       }
-      cache[this.state.searchTerm]["pages"][page] = result
+      cache[this.state.searchTerm]["pages"][page] = result;
     }
 
     this.setState({
       result: { hits: updatedHits, page, nbPages },
       cache: cache
-    })
+    });
   }
 
-  fetchSearchTopStories (searchTerm, currentPage) {
-    const { cache } = this.state
-    if (cache[searchTerm] && cache[searchTerm]["pages"][currentPage]) {
-      this.setSearchTopStories(cache[searchTerm]["pages"][currentPage])
-    } else {
-      fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${currentPage}&${PARAM_HPP}${DEFAULT_HPP}`)
-        .then(response => response.json())
-        .then(result => this.setSearchTopStories(result, true))
-        .catch(e => e)
-    }
+  fetchSearchTopStories(searchTerm, currentPage) {
+    const { cache } = this.state;
+
+    cache[searchTerm] && cache[searchTerm]["pages"][currentPage]
+      ? this.setSearchTopStories(cache[searchTerm]["pages"][currentPage])
+      : fetch(
+          `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${currentPage}&${PARAM_HPP}${DEFAULT_HPP}`
+        )
+          .then(response => response.json())
+          .then(result => this.setSearchTopStories(result, true))
+          .catch(e => this.setState({ error: e }));
   }
 
-  onDismiss (itemId) {
+  onDismiss(itemId) {
     const updatedHits = this.state.result.hits.filter(item => {
-      return item.objectID !== itemId
-    })
+      return item.objectID !== itemId;
+    });
 
     this.setState({
-      result: {...this.state.result, hits: updatedHits},
-    })
+      result: { ...this.state.result, hits: updatedHits }
+    });
   }
 
-  onSearchChange (event) {
-    this.setState({searchTerm: event.target.value})
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value });
   }
 
-  onSearchSubmit (event) {
-    this.setState({ currentPage: 0})
-    const { searchTerm } = this.state
-    this.fetchSearchTopStories(searchTerm, 0)
-    event.preventDefault()
+  onSearchSubmit(event) {
+    this.setState({ currentPage: 0 });
+    const { searchTerm } = this.state;
+    this.fetchSearchTopStories(searchTerm, 0);
+    event.preventDefault();
   }
 
-  onReset () {
+  onReset() {
     this.setState({
       result: [],
-      searchTerm: '',
-    })
+      searchTerm: ""
+    });
   }
 }
 
-export default App
+export default App;
