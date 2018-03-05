a859a14154ef4bd5c8617ac51afd04c14ecc73a5
Evis Bregu <evis.bregu@gmail.com>
2018-03-05 13:10:47 +0100

    Transforming to class component, testing refs

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,27 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Button from '../Button/';
 import PropTypes from 'prop-types';
 
-const Search = ({
-  searchTerm,
-  onSearchChange,
-  onSearchSubmit,
-  onReset,
-  children
-}) => (
-  <form onSubmit={onSearchSubmit}>
-    {children}
-    <input type="text" value={searchTerm} onChange={onSearchChange} />
-    <Button onClick={onReset}>RESET</Button>
-  </form>
-);
+class Search extends Component {
+  constructor (props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    if(this.textInput) {
+      this.textInput.focus();
+    }
+  }
+
+  render () {
+    const {searchTerm, onSearchChange, onSearchSubmit, onReset, children} = this.props;
+
+    return (
+      <form onSubmit={onSearchSubmit}>
+        {children}
+        <input type="text" value={searchTerm} onChange={onSearchChange}
+               ref={(input) => {this.textInput = input;}}/>
+        <Button onClick={onReset}>RESET</Button>
+      </form>
+    );
+  }
+}
 
 Search.propTypes = {
   searchTerm: PropTypes.string.isRequired,
   onSearchChange: PropTypes.func.isRequired,
   onSearchSubmit: PropTypes.func.isRequired,
   onReset: PropTypes.func.isRequired,
-  children: PropTypes.string
+  children: PropTypes.string,
 };
 
 export default Search;
