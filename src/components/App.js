e442df3f86c7cb6e64ab4d52c282924403a00393
Evis Bregu <evis.bregu@gmail.com>
2018-03-12 10:48:03 +0100

    Added higher order component to generate components with loading message

--- src/components/App.js
+++ src/components/App.js
@@ -5,15 +5,18 @@ import Search from './Search/';
 import Table from './Table/index';
 import './App.css';
 import Button from './Button/';
+import withLoading from '../hoc/withLoading';
 
 const DEFAULT_QUERY = 'redux';
-const DEFAULT_HPP = '100';
+const DEFAULT_HPP = '20';
 
 const PATH_BASE = 'https://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
 const PARAM_PAGE = 'page=';
 const PARAM_HPP = 'hitsPerPage=';
+
+const ButtonWithLoading = withLoading(Button);
 
 type Props = {
   foo: number
@@ -30,7 +33,8 @@ type State = {
   currentPage: number,
   result: Result,
   cache: Object,
-  error: ?Object
+  error: ?Object,
+  isLoading: boolean
 };
 
 class App extends Component<Props, State> {
@@ -42,7 +46,7 @@ class App extends Component<Props, State> {
   onReset: () => void;
   nextPage: () => void;
 
-  constructor(props: Props) {
+  constructor (props: Props) {
     super(props);
     this.state = {
       searchTerm: DEFAULT_QUERY,
@@ -51,9 +55,10 @@ class App extends Component<Props, State> {
       result: {
         hits: [],
         page: 0,
-        nbPages: 0
+        nbPages: 0,
       },
-      error: null
+      error: null,
+      isLoading: false,
     };
 
     this.setSearchTopStories = this.setSearchTopStories.bind(this);
@@ -65,14 +70,16 @@ class App extends Component<Props, State> {
     this.nextPage = this.nextPage.bind(this);
   }
 
-  componentDidMount() {
-    const { searchTerm, currentPage } = this.state;
+  componentDidMount () {
+    const {searchTerm, currentPage} = this.state;
     this.fetchSearchTopStories(searchTerm, currentPage);
   }
 
-  render() {
-    const { searchTerm, result, currentPage, error } = this.state;
+  render () {
+    const {searchTerm, result, currentPage, error, isLoading} = this.state;
     const totPages = result ? result.nbPages : 0;
+
+
 
     return (
       <div className="page">
@@ -102,86 +109,92 @@ class App extends Component<Props, State> {
         )}
 
         {currentPage < totPages && (
-          <Button onClick={this.nextPage}> More Articles </Button>
+
+          <ButtonWithLoading onClick={this.nextPage} isLoading={isLoading}>
+            More Articles
+          </ButtonWithLoading>
         )}
+
       </div>
     );
   }
 
-  nextPage() {
-    const { searchTerm, currentPage, result } = this.state;
+  nextPage () {
+    const {searchTerm, currentPage, result} = this.state;
     const totPages = result.nbPages;
 
     if (currentPage < totPages) {
       this.setState({
-        currentPage: currentPage + 1
+        currentPage: currentPage + 1,
       });
     }
 
     this.fetchSearchTopStories(searchTerm, currentPage + 1);
   }
 
-  setSearchTopStories(result: Result, updateCache: boolean) {
-    const { hits, page, nbPages } = result;
+  setSearchTopStories (result: Result, updateCache: boolean) {
+    const {hits, page, nbPages} = result;
     const oldHits = page !== 0 ? this.state.result.hits : [];
     const updatedHits = [...oldHits, ...hits];
 
-    let { cache } = this.state;
+    let {cache} = this.state;
     if (updateCache) {
       if (!cache[this.state.searchTerm]) {
-        cache[this.state.searchTerm] = { pages: [] };
+        cache[this.state.searchTerm] = {pages: []};
       }
       cache[this.state.searchTerm]['pages'][page] = result;
     }
 
     this.setState({
-      result: { hits: updatedHits, page, nbPages },
-      cache: cache
+      result: {hits: updatedHits, page, nbPages},
+      cache: cache,
+      isLoading: false,
     });
   }
 
-  fetchSearchTopStories(searchTerm: string, currentPage: number) {
-    const { cache } = this.state;
+  fetchSearchTopStories (searchTerm: string, currentPage: number) {
+    this.setState({isLoading: true});
+    const {cache} = this.state;
 
     cache[searchTerm] && cache[searchTerm]['pages'][currentPage]
       ? this.setSearchTopStories(cache[searchTerm]['pages'][currentPage], false)
       : fetch(
-          `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${currentPage}&${PARAM_HPP}${DEFAULT_HPP}`
-        )
-          .then(response => response.json())
-          .then(result => this.setSearchTopStories(result, true))
-          .catch(e => this.setState({ error: e }));
+      `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${currentPage}&${PARAM_HPP}${DEFAULT_HPP}`,
+      ).
+        then(response => response.json()).
+        then(result => this.setSearchTopStories(result, true)).
+        catch(e => this.setState({error: e}));
   }
 
-  onDismiss(itemId: number) {
+  onDismiss (itemId: number) {
     const updatedHits = this.state.result.hits.filter(item => {
       return item.objectID !== itemId;
     });
 
     this.setState({
-      result: { ...this.state.result, hits: updatedHits }
+      result: {...this.state.result, hits: updatedHits},
     });
   }
 
-  onSearchChange(event: Object) {
-    this.setState({ searchTerm: event.target.value });
+  onSearchChange (event: Object) {
+    this.setState({searchTerm: event.target.value});
   }
 
-  onSearchSubmit(event: Object) {
-    this.setState({ currentPage: 0 });
-    const { searchTerm } = this.state;
+  onSearchSubmit (event: Object) {
+    this.setState({currentPage: 0});
+    const {searchTerm} = this.state;
     this.fetchSearchTopStories(searchTerm, 0);
     event.preventDefault();
   }
 
-  onReset() {
+  onReset () {
     this.setState({
       result: {
         hits: [],
         page: 0,
-        nbPages: 0
+        nbPages: 0,
       },
-      searchTerm: ''
+      searchTerm: '',
     });
   }
 }
