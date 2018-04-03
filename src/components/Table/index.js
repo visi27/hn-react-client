eed76e353c628f9919df28f270ba443c6d9c9749
Evis Bregu <evis.bregu@gmail.com>
2018-04-03 14:51:20 +0200

    connecting Table component to redux store, mapping state

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -3,6 +3,7 @@ import Item from '../Item/index';
 import Header from '../Header/index';
 import PropTypes from 'prop-types';
 import { sortBy } from 'lodash';
+import { connect } from 'react-redux'
 
 const SORTS = {
   NONE: list => list,
@@ -15,10 +16,18 @@ const SORTS = {
 class Table extends Component {
 
   render () {
+    const { result } = this.props;
+    console.log(result);
     return (
       <div className="table">
         <Header />
-
+        {
+          result.hits.map((item, key) => {
+            return (
+              <Item key={key} item={item}/>
+            )
+          })
+        }
           {/*<Item key={item.objectID} item={item} onDismiss={onDismiss}/>*/}
 
       </div>
@@ -26,4 +35,10 @@ class Table extends Component {
   }
 }
 
-export default Table;
+function mapStateToProps (state) {
+  const { result } = state;
+
+  return { result }
+}
+
+export default connect(mapStateToProps, null)(Table)
