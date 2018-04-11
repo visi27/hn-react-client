93215b16d2ae9c5ddc1290f288e9a1f2f53fe6fe
Evis Bregu <evis.bregu@gmail.com>
2018-04-11 12:03:48 +0200

    Reorganising imports

--- src/components/Search/spec.js
+++ src/components/Search/spec.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import Search from './presenter';
-import { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import { configure } from 'enzyme';
+import { configure, shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 
 configure({ adapter: new Adapter() });
