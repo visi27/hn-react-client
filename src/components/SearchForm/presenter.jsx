e163d0db688799e53c512f0b1e0b15582f2f1e12
Evis Bregu <evis.bregu@gmail.com>
2018-04-17 16:12:19 +0200

    Search component for header navbar

---
+++ src/components/SearchForm/presenter.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SearchForm = () => (
+  <form className="form-inline my-2 my-lg-0">
+    <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
+    <button className="btn btn-outline-success my-2 my-sm-0" type="submit">
+      Search
+    </button>
+  </form>
+);
+
+export default SearchForm;
