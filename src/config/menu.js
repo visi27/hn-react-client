196b85334e5438d6444b941e05b4aadcac89eb02
Evis Bregu <evis.bregu@gmail.com>
2018-04-18 15:14:53 +0200

    Menu config file

---
+++ src/config/menu.js
@@ -0,0 +1,32 @@
+import * as menuTypes from '../constants/constants';
+
+export default [
+  {
+    title: 'Menu',
+    type: menuTypes.LINK_MENU_TYPE,
+    href: '#',
+  },
+  {
+    title: 'Another Menu',
+    type: menuTypes.LINK_MENU_TYPE,
+    href: '#',
+  },
+  {
+    title: 'Dropdown',
+    type: menuTypes.DROPDOWN_MENU_TYPE,
+    href: '#',
+    id: 'dd01',
+    children: [
+      {
+        title: 'SubMenu',
+        type: menuTypes.LINK_MENU_TYPE,
+        href: '#',
+      },
+      {
+        title: 'Another SubMenu',
+        type: menuTypes.LINK_MENU_TYPE,
+        href: '#',
+      },
+    ],
+  },
+];
