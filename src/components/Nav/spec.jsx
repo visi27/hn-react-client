b9a7233170c6375012312fe77a5d316d586ce7a2
Evis Bregu <evis.bregu@gmail.com>
2018-04-18 16:00:57 +0200

    Nav component tests

---
+++ src/components/Nav/spec.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import renderer from 'react-test-renderer';
+import { mount, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Nav from './presenter';
+import * as menuTypes from '../../constants/constants';
+
+configure({ adapter: new Adapter() });
+
+describe('Nav', () => {
+  const props = {
+    elements: [
+      {
+        title: 'Menu',
+        type: menuTypes.LINK_MENU_TYPE,
+        href: '#',
+      },
+      {
+        title: 'Another Menu',
+        type: menuTypes.LINK_MENU_TYPE,
+        href: '#',
+      },
+      {
+        title: 'Dropdown',
+        type: menuTypes.DROPDOWN_MENU_TYPE,
+        href: '#',
+        id: 'dd01',
+        children: [
+          {
+            title: 'SubMenu',
+            type: menuTypes.LINK_MENU_TYPE,
+            href: '#',
+          },
+          {
+            title: 'Another SubMenu',
+            type: menuTypes.LINK_MENU_TYPE,
+            href: '#',
+          },
+        ],
+      },
+    ],
+  };
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDom.render(<Nav {...props} />, div);
+  });
+
+  test('has valid snapshot', () => {
+    const component = renderer.create(<Nav {...props} />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('shows 3 menu items', () => {
+    const element = mount(<Nav {...props} />);
+    expect(element.find('.nav-item').length).toBe(3);
+  });
+
+  it('shows 1 dropdown menu ', () => {
+    const element = mount(<Nav {...props} />);
+    expect(element.find('.nav-item .dropdown').length).toBe(1);
+  });
+});
