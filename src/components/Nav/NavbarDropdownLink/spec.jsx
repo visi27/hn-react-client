141f2178b69ad7c3e195e781caaa1e9cb6337eb8
Evis Bregu <evis.bregu@gmail.com>
2018-04-19 11:38:11 +0200

    NavbarDropdownLink tests

---
+++ src/components/Nav/NavbarDropdownLink/spec.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import renderer from 'react-test-renderer';
+import { mount, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import NavbarDropdownLink from './presenter';
+import * as menuTypes from '../../../constants/menu';
+
+configure({ adapter: new Adapter() });
+
+describe('Nav', () => {
+  const props = {
+    dropdown: {
+      title: 'Dropdown',
+      type: menuTypes.DROPDOWN_MENU_TYPE,
+      href: '#',
+      id: 'dd01',
+      children: [
+        {
+          title: 'SubMenu',
+          type: menuTypes.LINK_MENU_TYPE,
+          href: '#',
+        },
+        {
+          title: 'Another SubMenu',
+          type: menuTypes.LINK_MENU_TYPE,
+          href: '#',
+        },
+      ],
+    },
+  };
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDom.render(<NavbarDropdownLink {...props} />, div);
+  });
+
+  test('has valid snapshot', () => {
+    const component = renderer.create(<NavbarDropdownLink {...props} />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('shows 2 dropdown items', () => {
+    const element = mount(<NavbarDropdownLink {...props} />);
+    expect(element.find('a.dropdown-item').length).toBe(2);
+  });
+
+  it('shows 1 dropdown menu ', () => {
+    const element = mount(<NavbarDropdownLink {...props} />);
+    expect(element.find('.nav-item .dropdown').length).toBe(1);
+  });
+});
