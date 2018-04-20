1f53805f7357a8010794f1fd1376225f60a8baca
Evis Bregu <evis.bregu@gmail.com>
2018-04-20 11:44:00 +0200

    Throttling persist to localstorage with lodash throttle

--- src/index.dev.jsx
+++ src/index.dev.jsx
@@ -4,11 +4,14 @@ import ReactDOM from 'react-dom';
 // import { Router, Route, IndexRoute, browserHistory } from "react-router";
 // import { syncHistoryWithStore } from "react-router-redux";
 import { Provider } from 'react-redux';
+import throttle from 'lodash/throttle';
 import configureStore from './stores/configureStore';
 import ConnectedApp from './components/App';
 import './index.css';
 import DevTools from './containers/DevTools';
 import registerServiceWorker from './registerServiceWorker';
+
+import * as appLocalStorage from './modules/localStorage';
 
 import '../node_modules/jquery/dist/jquery.slim';
 import '../node_modules/popper.js/dist/umd/popper';
@@ -16,7 +19,12 @@ import '../node_modules/bootstrap/js/dist/util';
 import '../node_modules/bootstrap/js/dist/collapse';
 import './scss/index.scss';
 
-const store = configureStore();
+const store = configureStore(appLocalStorage.loadState());
+
+store.subscribe(throttle(() => {
+  // Persist only user data, not the whole state
+  appLocalStorage.persistState({ search: store.getState().search });
+}, 2000));
 
 ReactDOM.render(
   <Provider store={store}>
