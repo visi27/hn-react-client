401ddc6cf8978c1f5f101c47e3abff44115c56fb
Evis Bregu <evis.bregu@gmail.com>
2018-04-23 14:31:24 +0200

    Continuing bootstrap design implementation

--- src/components/Table/Item/index.jsx
+++ src/components/Table/Item/index.jsx
@@ -1,23 +1,32 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
 import Button from '../../Button/presenter';
-import * as style from './style';
 
 const Item = ({ item, onDismiss }) => (
-  <div key={item.objectID} className="container-fluid">
-    <span style={style.largeColumn}>
-      <a href={item.url}>{item.title}</a>
-    </span>
+  <div key={item.objectID} className="container-fluid item">
     <div className="list-inline">
-      <span className="list-inline-item">Author: {item.author}</span>
-      <span className="list-inline-item">Comments: {item.num_comments}</span>
-      <span className="list-inline-item">Points: {item.points}</span>
+      <span className="list-inline-item">
+        <a className="h5 text-muted" href={item.url}>
+          {item.title}
+        </a>
+      </span>
       <span className="list-inline-item">
         <Button className="btn btn-outline-danger btn-sm" onClick={() => onDismiss(item.objectID)}>
           Dismiss
         </Button>
       </span>
     </div>
+    <div className="list-inline">
+      <span className="list-inline-item">Author: {item.author}</span>
+      <span className="list-inline-item">Comments: {item.num_comments}</span>
+      <span className="list-inline-item">Points: {item.points}</span>
+      <span className="list-inline-item">
+        <a className="text-muted" href={item.url}>
+          ({item.url})
+        </a>
+      </span>
+    </div>
+    <hr />
   </div>
 );
 
