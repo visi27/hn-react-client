d94ca4949ece7e9c6362f5083e4f7e5ae9abc188
Evis Bregu <evis.bregu@gmail.com>
2018-05-02 11:23:23 +0200

    fetchData action creator now acccepts an object as param. Added Paginator action creators

--- src/actions/result.js
+++ src/actions/result.js
@@ -6,7 +6,7 @@ export const receiveResults = results => ({
   results,
 });
 
-export function fetchData(query = conf.DEFAULT_QUERY, page = 1) {
+export function fetchData({ query = conf.DEFAULT_QUERY, page = 1 }) {
   return (dispatch) => {
     const url = `https://hn.algolia.com/api/v1/search?query=${query}&tags=story&page=${page}`;
     // dispatch(itemsIsLoading(true));
@@ -23,3 +23,27 @@ export function fetchData(query = conf.DEFAULT_QUERY, page = 1) {
       .catch(() => console.log('ERROR'));
   };
 }
+
+export const nextPage = () => (dispatch, getState) => {
+  const curPage = Number.parseInt(getState().result.page, 10);
+  const totPages = Number.parseInt(getState().result.nbPages, 10);
+
+  if (curPage < totPages) {
+    dispatch(fetchData({ page: curPage + 1 }));
+  }
+};
+
+export const prevPage = () => (dispatch, getState) => {
+  if (Number.parseInt(getState().result.page, 10) > 1) {
+    dispatch(fetchData({ page: Number.parseInt(getState().result.page, 10) - 1 }));
+  }
+};
+
+export const goToPage = page => (dispatch, getState) => {
+  if (
+    Number.parseInt(page, 10) < Number.parseInt(getState().result.nbPages, 10) &&
+    Number.parseInt(page, 10) > 0
+  ) {
+    dispatch(fetchData({ page }));
+  }
+};
