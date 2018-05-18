adc10ff89beb3a6cbdfe65d583de41ff17250f8d
Evis Bregu <evis.bregu@gmail.com>
2018-05-18 11:22:55 +0200

    Using https for API calls

--- src/_config/api.js
+++ src/_config/api.js
@@ -1,7 +1,7 @@
 const apiConfig = {
-  base_url: 'http://api.evisbregu.com/v2.0',
-  get_token_url: 'http://api.evisbregu.com/tokens',
-  get_user_url: 'http://api.evisbregu.com/user',
+  base_url: 'https://api.evisbregu.com/v2.0',
+  get_token_url: 'https://api.evisbregu.com/tokens',
+  get_user_url: 'https://api.evisbregu.com/user',
 };
 
 export default apiConfig;
