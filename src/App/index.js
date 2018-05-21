b4f847ac0aabc258500c321407327f0587fb7612
Evis Bregu <evis.bregu@gmail.com>
2018-05-21 14:33:31 +0200

    Remove dispatch loadMenu from onLoad. Menu will be loaded directly from NavBar

--- src/App/index.js
+++ src/App/index.js
@@ -1,11 +1,9 @@
 import { connect } from 'react-redux';
-import loadMenu from '../_actions/menu';
 import { fetchData } from '../_actions/result';
 import App from './presenter';
 
 const mapDispatchToProps = dispatch => ({
   onLoad() {
-    dispatch(loadMenu());
     dispatch(fetchData({}));
   },
   onLocationChange() {
